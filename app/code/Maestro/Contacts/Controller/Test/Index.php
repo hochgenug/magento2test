07ec765c4d53a737fea3b2dff3659386847ea71c
Julien Hochgenug <hochgenug@gmail.com>
2016-11-20 21:33:21 +0100

    contacts module test

---
+++ app/code/Maestro/Contacts/Controller/Test/Index.php
@@ -0,0 +1,9 @@
+<?php
+namespace Maestro\Contacts\Controller\Test;
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        die('test index');
+    }
+}
