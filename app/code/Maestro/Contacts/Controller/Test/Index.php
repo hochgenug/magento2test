50ee1f202d2d86d4e45bc6b36ecf304da8287dec
Julien Hochgenug <hochgenug@gmail.com>
2016-11-20 22:04:34 +0100

    block & template

--- app/code/Maestro/Contacts/Controller/Test/Index.php
+++ app/code/Maestro/Contacts/Controller/Test/Index.php
@@ -4,7 +4,9 @@ class Index extends \Magento\Framework\App\Action\Action
 {
     public function execute()
     {
+        // charge le layout
         $this->_view->loadLayout();
+        // fait le rendu
         $this->_view->renderLayout();
     }
 }
